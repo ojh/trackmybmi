d9a2fc01c2b81ae45f6ce81f5dc360f7f9ef8e36
Olivier Harris <o.harris@kabucreative.com.au>
2015-01-14 15:33:22 +1100

    Simplify unit test

--- trackmybmi/users/tests/test_models.py
+++ trackmybmi/users/tests/test_models.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.hashers import make_password
 from django.test import TestCase
 
 
@@ -7,7 +6,7 @@ User = get_user_model()
 
 class UserTest(TestCase):
     def test_can_login_with_email(self):
-        password = make_password('testpassword')
-        user = User.objects.create(email='test@test.test', password=password)
-        logged_in = self.client.login(username=user.email, password='testpassword')
+        user = User.objects.create_user('test@test.test', 'testpassword')
+        logged_in = self.client.login(username=user.email,
+                                      password='testpassword')
         self.assertTrue(logged_in)
