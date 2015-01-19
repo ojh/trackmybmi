558fe07277263cdd5f13966427777c476204fb2c
Olivier Harris <o.harris@kabucreative.com.au>
2015-01-19 13:55:42 +1100

    Ensure only one measurement can be logged against any one user per day

--- trackmybmi/measurements/models.py
+++ trackmybmi/measurements/models.py
@@ -19,6 +19,9 @@ class Measurement(models.Model):
     weight = models.DecimalField(max_digits=5, decimal_places=2,
                                  help_text="Mass in kilograms")
 
+    class Meta:
+        unique_together = (('user', 'date'),)
+
     def __str__(self):
         return "{} - {}".format(self.user.email, self.date.strftime('%Y-%m-%d'))
 
--- trackmybmi/measurements/tests/test_models.py
+++ trackmybmi/measurements/tests/test_models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from measurements.factories import MeasurementFactory
@@ -13,6 +14,14 @@ class MeasurementTest(TestCase):
         date = datetime.date(2015, 1, 1)
         measurement = MeasurementFactory(user=user, date=date)
         self.assertEqual(measurement.__str__(), "test@test.test - 2015-01-01")
+
+    def test_users_can_only_have_one_measurement_per_date(self):
+        user = UserFactory(email='test@test.test')
+        date = datetime.date(2015, 1, 1)
+        measurement = MeasurementFactory(user=user, date=date)
+        with self.assertRaises(ValidationError):
+            same_user_same_day = MeasurementFactory(user=user, date=date)
+            same_user_same_day.full_clean()
 
     def test_calculate_bmi(self):
         measurement = MeasurementFactory(height=1.74, weight=65.0)
